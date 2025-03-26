e33b1d2d552a15ed2ceee530771781f88fe9cb90
RGTripleA <roberto.gonzalez@triplea.cl>
2025-03-26 15:45:43 -0300

    Cambios para ingreso de user

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      BASE_URL:
-        process.env.NODE_ENV === "production"
-          ? process.env.NEXT_PUBLIC_BASE_URL
-          : "http://tu-url.com",
-    },
-  
-    // Cambio del directorio de construcción
-    distDir: "dist",
-  
-    // Cambio del directorio de salida
-    output: "export",
-  
-    // Configuración de imágenes (unoptimized:true deshabilita la optimización automática de imágenes)
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  env: {
+    BASE_URL:
+      process.env.NODE_ENV === "production"
+        ? process.env.NEXT_PUBLIC_BASE_URL
+        : "http://d-dkr-betel.tricotcorp.cl:8080/oms-service/v1",
+  },
+
+  // Cambio del directorio de construcción
+  distDir: "dist",
+
+  // Cambio del directorio de salida
+  output: "export",
+
+  // Configuración de imágenes (unoptimized:true deshabilita la optimización automática de imágenes)
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
+
